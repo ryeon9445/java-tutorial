992dd02b086b18936f2f0a2b62af8f8fea2466be
ryeon9445 <ryeon9445@gmail.com>
2019-05-24 09:05:10 +0900

    lambda tutorial

--- lambda/src/test/java/com/cbr/java/lambda/ReduceTest.java
+++ lambda/src/test/java/com/cbr/java/lambda/ReduceTest.java
@@ -1,5 +1,38 @@
 package com.cbr.java.lambda;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
 public class ReduceTest {
 
+    @Test
+    public void testSum() {
+        int  totalOfCharactersInAllFriends= Data.FRIENDS.stream()
+                .mapToInt(String::length)
+                .sum();
+        Assert.assertEquals(26, totalOfCharactersInAllFriends);
+    }
+
+    @Test
+    public void testReduce() {
+        final Optional<String> aLongName = Data.FRIENDS.stream()
+                .reduce((friends1, friends2) ->
+                        friends1.length() >= friends2.length() ? friends1 : friends2);
+
+        aLongName.ifPresent(friend ->
+                System.out.println(String.format("A longest name: %s", friend)));
+
+        Assert.assertEquals("Brian", aLongName.orElseGet(String::new));
+    }
+
+    @Test
+    public void testReduce2() {
+        final String steveOrLonger = Data.FRIENDS.stream()
+                .reduce("Steve", (friend1, friend2) ->
+                        friend1.length() >= friend2.length() ? friend1 : friend2);
+
+        Assert.assertEquals("Steve", steveOrLonger);
+    }
 }
