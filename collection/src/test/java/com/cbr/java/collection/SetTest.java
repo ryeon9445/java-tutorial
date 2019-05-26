9e84de368e2a4b3efcdb5ad93243e3c09419e5ad
ryeon9445 <ryeon9445@gmail.com>
2019-05-26 23:09:03 +0900

    collection tutorial

---
+++ collection/src/test/java/com/cbr/java/collection/SetTest.java
@@ -0,0 +1,49 @@
+package com.cbr.java.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+public class SetTest {
+
+    @Test
+    public void testHashSet() {
+        HashSet<String> strings = new HashSet<>();
+
+        strings.add("aaa");
+        strings.add("bbb");
+        strings.add("ccc");
+        strings.add("ccc");
+
+        Assert.assertEquals(3, strings.size());
+    }
+
+    @Test
+    public void testHashSetOfObject() {
+        HashSet<CustomString> customStrings = new HashSet<>();
+        customStrings.add(new CustomString("aaa"));
+        customStrings.add(new CustomString("aaa"));
+
+        Assert.assertEquals(2, customStrings.size());
+    }
+
+    private static class CustomString {
+
+        public CustomString(String name) {
+            this.name = name;
+        }
+
+        private String name;
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+//        @Override
+//        public boolean equals(Object obj) {
+//            if (obj instanceof )
+//        }
+    }
+}
