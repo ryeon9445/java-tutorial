f852c13c09ef9c433c5512f3fa14141fba8ab3fa
ryeon9445 <ryeon9445@gmail.com>
2019-05-31 20:03:18 +0900

    lambda tutorial

--- lambda/src/test/java/com/cbr/java/lambda/FileTest.java
+++ lambda/src/test/java/com/cbr/java/lambda/FileTest.java
@@ -1,12 +1,17 @@
 package com.cbr.java.lambda;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileTest {
 
@@ -35,5 +40,27 @@ public class FileTest {
     public void testFileOfHidden() {
         final File[] files = new File(".").listFiles(File::isHidden);
         Arrays.stream(files).forEach(System.out::println);
+    }
+
+    @Test
+    public void testFileList() {
+        List<File> files = new ArrayList<>();
+
+        File[] filesInCurrentDir = new File(".").listFiles();
+        for (File file : filesInCurrentDir) {
+            File[] filesInSubDir = file.listFiles();
+            if (filesInSubDir != null) {
+                files.addAll(Arrays.asList(filesInSubDir));
+            } else {
+                files.add(file);
+            }
+        }
+
+        List<File> files2 = Stream.of(new File(".").listFiles())
+                .flatMap(file -> file.listFiles() == null ? Stream.of(file) : Stream.of(file.listFiles()))
+                .collect(Collectors.toList());
+
+
+        Assert.assertEquals(files.size(), files2.size());
     }
 }
